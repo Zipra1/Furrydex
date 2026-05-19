e1540546a9c273cacb71a885f9ffc9d80db376e1
Zipra <zippyzipra@gmail.com>
2026-05-18 20:36:55 -0400

    Documentation template

--- src/imgdata.h
+++
@@ -1,3 +0,0 @@
-extern unsigned char IMAGE_DATA[];
-extern unsigned char IMAGE_DATA2[];
-extern unsigned char lua_buffer[];
--- src/luazephyrlib.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUAZEPHYRLIB_H
-#define LUAZEPHYRLIB_H
-
-#include <zephyr/kernel.h>
-
-extern struct k_mutex paint_mutex;
-
-#endif
