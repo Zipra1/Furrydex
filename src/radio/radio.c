081e52532c03cedaa9589da54774f6a2c1965873
Zipra <zippyzipra@gmail.com>
2026-08-25 17:28:07 -0400

    Switch from legacy to extended advertizing

--- src/radio/radio.c
+++ src/radio/radio.c
@@ -1,5 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
 
 // initialization //
 
@@ -34,10 +35,44 @@ static const struct bt_data ad[] = {
     BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, sizeof(mfg_data)),
 };
 
+static struct bt_le_ext_adv *streetpass_adv;
+
+// Supposedly S=2, dunno if Zephyr can do S=8
+static const struct bt_le_adv_param adv_param = {
+    .options      = BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CODED,
+    .interval_min = BT_GAP_ADV_SLOW_INT_MIN,
+    .interval_max = BT_GAP_ADV_SLOW_INT_MAX,
+};
+
 int streetpass_adv_start(void) {
-    return bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), NULL, 0);
+    int err;
+
+    err = bt_le_ext_adv_create(&adv_param, NULL, &streetpass_adv);
+    if (err) {
+        printk("ext_adv_create failed (err %d)\n", err);
+        return err;
+    }
+
+    err = bt_le_ext_adv_set_data(streetpass_adv, ad, ARRAY_SIZE(ad), NULL, 0);
+    if (err) {
+        printk("ext_adv_set_data failed (err %d)\n", err);
+        return err;
+    }
+
+    err = bt_le_ext_adv_start(streetpass_adv, BT_LE_EXT_ADV_START_DEFAULT);
+    if (err) {
+        printk("ext_adv_start failed (err %d)\n", err);
+        return err;
+    }
+
+    printk("Streetpass advertising started (Coded PHY)\n");
+    return 0;
 }
 
 void streetpass_adv_stop(void) {
-    bt_le_adv_stop();
+    if (streetpass_adv) {
+        bt_le_ext_adv_stop(streetpass_adv);
+        bt_le_ext_adv_delete(streetpass_adv);
+        streetpass_adv = NULL;
+    }
 }
