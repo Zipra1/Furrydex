c063ac830d9d6e963cd7b0295f032dbfe00b19da
Zipra <zippyzipra@gmail.com>
2026-08-29 13:50:49 -0400

    - Reorganize radio.c

--- src/radio/radio.c
+++ src/radio/radio.c
@@ -27,28 +27,39 @@ int ble_core_init(void)
     return err;
 }
 
-// streetpass //
+// scanning //
 
-#define FURRYDEX_COMPANY_ID 0xFFFF // use real/registered ID before shipping
+static void scan_recv(const struct bt_le_scan_recv_info *info, struct net_buf_simple *buf)
+{
+    struct ble_scan_event ev;
 
-static uint8_t mfg_data[] = {
-    FURRYDEX_COMPANY_ID & 0xFF, FURRYDEX_COMPANY_ID >> 8,
-    'F', 'D',
-    6, 2, 1, 4, 2, 0, 6, 9 // unique ID
+    ev.addr = *info->addr;
+    ev.rssi = info->rssi;
+    ev.ad_len = MIN(buf->len, BLE_MAX_AD_LEN);
+    memcpy(ev.ad_data, buf->data, ev.ad_len);
+
+    if (buf->len > BLE_MAX_AD_LEN)
+    {
+        printk("ble: advertizement truncated (%u > %u bytes)\n", buf->len, BLE_MAX_AD_LEN);
+    }
+    ble_fifo_put(ev);
+}
+
+static struct bt_le_scan_cb ble_scan_callbacks = {
+    .recv = scan_recv,
 };
 
-static const struct bt_data ad[] = {
-    BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, sizeof(mfg_data)),
-};
-
-static struct bt_le_ext_adv *streetpass_adv;
-
-// Supposedly S=2, dunno if Zephyr can do S=8
-static const struct bt_le_adv_param adv_param = {
-    .options = BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CODED,
-    .interval_min = BT_GAP_ADV_SLOW_INT_MIN,
-    .interval_max = BT_GAP_ADV_SLOW_INT_MAX,
-};
+int ble_scan_start(void)
+{
+    struct bt_le_scan_param scan_param = {
+        .type = BT_LE_SCAN_TYPE_PASSIVE,
+        .options = BT_LE_SCAN_OPT_CODED | BT_LE_SCAN_OPT_NO_1M,
+        .interval = BT_GAP_SCAN_FAST_INTERVAL,
+        .window = BT_GAP_SCAN_FAST_WINDOW,
+    };
+    bt_le_scan_cb_register(&ble_scan_callbacks);
+    return bt_le_scan_start(&scan_param, NULL);
+}
 
 static int ble_fifo_in;
 static int ble_fifo_out;
@@ -124,6 +135,29 @@ int ble_fifo_get(struct ble_scan_event *pulled_item)
     return 0;
 }
 
+// advertizing //
+
+#define FURRYDEX_COMPANY_ID 0xFFFF // use real/registered ID before shipping
+
+static uint8_t mfg_data[] = {
+    FURRYDEX_COMPANY_ID & 0xFF, FURRYDEX_COMPANY_ID >> 8,
+    'F', 'D',
+    6, 2, 1, 4, 2, 0, 6, 9 // unique ID
+};
+
+static const struct bt_data ad[] = {
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, sizeof(mfg_data)),
+};
+
+static struct bt_le_ext_adv *streetpass_adv;
+
+// Supposedly S=2, dunno if Zephyr can do S=8
+static const struct bt_le_adv_param adv_param = {
+    .options = BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CODED,
+    .interval_min = BT_GAP_ADV_SLOW_INT_MIN,
+    .interval_max = BT_GAP_ADV_SLOW_INT_MAX,
+};
+
 int streetpass_adv_start(void)
 {
     int err;
@@ -161,38 +195,4 @@ void streetpass_adv_stop(void)
         bt_le_ext_adv_delete(streetpass_adv);
         streetpass_adv = NULL;
     }
-}
-
-K_MSGQ_DEFINE(ble_scan_msgq, sizeof(struct ble_scan_event), 8, 1);
-
-static void scan_recv(const struct bt_le_scan_recv_info *info, struct net_buf_simple *buf)
-{
-    struct ble_scan_event ev;
-
-    ev.addr = *info->addr;
-    ev.rssi = info->rssi;
-    ev.ad_len = MIN(buf->len, BLE_MAX_AD_LEN);
-    memcpy(ev.ad_data, buf->data, ev.ad_len);
-
-    if (buf->len > BLE_MAX_AD_LEN)
-    {
-        printk("ble: advertizement truncated (%u > %u bytes)\n", buf->len, BLE_MAX_AD_LEN);
-    }
-    ble_fifo_put(ev);
-}
-
-static struct bt_le_scan_cb ble_scan_callbacks = {
-    .recv = scan_recv,
-};
-
-int ble_scan_start(void)
-{
-    struct bt_le_scan_param scan_param = {
-        .type = BT_LE_SCAN_TYPE_PASSIVE,
-        .options = BT_LE_SCAN_OPT_CODED | BT_LE_SCAN_OPT_NO_1M,
-        .interval = BT_GAP_SCAN_FAST_INTERVAL,
-        .window = BT_GAP_SCAN_FAST_WINDOW,
-    };
-    bt_le_scan_cb_register(&ble_scan_callbacks);
-    return bt_le_scan_start(&scan_param, NULL);
 }
